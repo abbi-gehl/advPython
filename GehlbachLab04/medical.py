8969bf913a56c2f85443e5f956caf1ce2ed8de99
Abbi Gehlbach <abbi.gehlbach@gmail.com>
2025-02-13 20:55:36 -0500

    sln

--- GehlbachLab04/medical.py
+++ GehlbachLab04/medical.py
@@ -1,6 +1,22 @@
-# Abigail Gehlbach
-# lab 04 supporting file
+"""
+Medical.py file for patient and procedure classes delcaration
+uses pickle to save information to save.p file and load when opened again
 
+Author: Abigail Gehlbach
+Class: CSI-260-01
+Assignment: Week 4 Lab
+Due Date: February 13, 2019 11:59 PM
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+"""
 import pickle
 
 
@@ -10,28 +26,27 @@ class Patient:
 
     @classmethod
     def get_patient(cls, pid):
-        try:
-            return cls._all_patients[pid]
-        except KeyError:
-            return None
-        except TypeError:
-            return None
-        else:
-            return None
+        return cls._all_patients.get(pid, None)
 
     @classmethod
     def delete_patient(cls, pid):
-        try:
+        if pid in cls._all_patients:
             del cls._all_patients[pid]
-            return
-        except KeyError:
-            return None
-        else:
-            return None
-
+    
     @classmethod
     def save_patients(cls):
         pickle.dump(cls._all_patients, open("save.p", "wb"))
+
+    @classmethod
+    def load_patients(cls):
+        try:
+            with open("save.p", "rb") as f:
+                cls._all_patients = pickle.load(f)
+            if cls._all_patients:
+                cls._next_id = max(cls._all_patients.keys()) + 1
+        except (FileNotFoundError, EOFError):
+            cls._all_patients = {}
+            cls._next_id = 100
 
     def __init__(self, first_name, last_name, address, phone_number, emergency_contact_name, emergency_contact_num):
         self.first_name = first_name
@@ -61,6 +76,11 @@ class Patient:
 class Procedure:
     _next_id = 1000
 
+    @classmethod
+    def update_next_id(cls, patients):
+        max_proc_id = max((proc._id for p in patients.values() for proc in p.procedures), default=999)
+        cls._next_id = max_proc_id + 1
+
     def __init__(self, name, doctor, date, cost):
         self.name = name
         self.doctor = doctor
@@ -73,18 +93,6 @@ class Procedure:
         return(f"Procedure {self._id}: {self.name} | Doctor: {self.doctor}\n"
                f"Date: {self.date} | Cost: {self.cost}")
 
-
-p1 = Patient("Abigail", "Gehlbach", "123 abc street", "111-222-3345", "my dad", "123-333-2314")
-print(p1)
-
-p2 = Patient("Clare", "McCaffrey", "456 xyz rd", "333-371-2373", "her dad", "152-628-4871")
-print(p2)
-
-proc1 = Procedure("lung extensions", "Dr. Crowbar", "4/20/2025", 100000.87)
-proc2 = Procedure("new tummy", "Dr. tummy", "3/21/2025", 20000.55)
-
-p1.add_procedure(proc1)
-p2.add_procedure(proc2)
-
-print(Patient.get_patient(100))
-print(Patient.get_patient(101))
+def initialize_data():
+    Patient.load_patients()
+    Procedure.update_next_id(Patient._all_patients)
--- GehlbachLab04/week4.py
+++ GehlbachLab04/week4.py
@@ -1,8 +1,28 @@
+"""
+Driver file for medical.py, takes user input and makes calls to medical.py
+
+Author: Abigail Gehlbach
+Class: CSI-260-01
+Assignment: Week 4 Lab
+Due Date: February 13, 2019 11:59 PM
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+"""
+
 import medical
 
+medical.initialize_data()
 
 def get_choice():
-    # get input, no cleaning
+    """get user input, no data cleaning"""
     return input("1. Look up a patient by ID number.\n"
                  "2. Add a new patient\n"
                  "3. Quit\n"
@@ -10,50 +30,99 @@ def get_choice():
 
 
 def create_patient():
-    person_list =
+    """creates new patient"""
+    first_name = input("Enter first name: ")
+    last_name = input("Enter last name: ")
+    address = input("Enter address: ")
+    phone_number = input("Enter phone number: ")
+    emergency_contact_name = input("Enter emergency contact name: ")
+    emergency_contact_num = input("Enter emergency contact number: ")
+    
+    new_patient = medical.Patient(first_name, last_name, address, phone_number, emergency_contact_name, emergency_contact_num)
+    print(f"New patient added with ID {new_patient._id}")
 
 
-def modify_patient():
-    input("1. Change patient name\n"
-          "2. Change patient address\n"
-          "3. Change patient phone\n"
-          "4. Change emergency contact name\n"
-          "Input choice: ")
+def modify_patient(patient):
+    """allows user to modify patient"""
+    choice = input("a. Change patient name\n"
+                   "b. Change patient address\n"
+                   "c. Change patient phone\n"
+                   "d. Change emergency contact name\n"
+                   "e. Change emergency contact number\n"
+                   "Input choice: ")
+    
+    if choice == 'a':
+        patient.first_name = input("Enter new first name: ")
+        patient.last_name = input("Enter new last name: ")
+    elif choice == 'b':
+        patient.address = input("Enter new address: ")
+    elif choice == 'c':
+        patient.phone_number = input("Enter new phone number: ")
+    elif choice == 'd':
+        patient.emergency_contact_name = input("Enter new emergency contact name: ")
+    elif choice == 'e':
+        patient.emergency_contact_num = input("Enter new emergency contact number: ")
+    else:
+        print("Invalid choice [a-e]")
 
 
+def add_procedure(patient):
+    """adds procedure to patient"""
+    name = input("Enter procedure name: ")
+    doctor = input("Enter doctor's name: ")
+    date = input("Enter procedure date: ")
+    cost = float(input("Enter procedure cost: "))
+    
+    procedure = medical.Procedure(name, doctor, date, cost)
+    patient.add_procedure(procedure)
+    print(f"Procedure {procedure._id} added to patient {patient._id}")
 
 
 def patient_lookup():
+    """patient lookup, allows for patient alterations if id lookup successful"""
     pid = input("Enter patient ID number: ")
     patient = medical.Patient.get_patient(pid)
 
+    try:
+        pid = int(pid)
+        patient = medical.Patient.get_patient(pid)
+    except ValueError:
+        print("Invalid ID format")
+        return
+    
     if patient:
+        print(patient)
         while True:
-            patient_option = input("(a) modify a patient's attributes, "
-                                   "(b) delete a patient, or "
-                                   "(c) add a procedure"
+            patient_option = input("a. Modify a patient's attributes\n"
+                                   "b. Delete a patient\n"
+                                   "c. Add a procedure\n"
+                                   "d. Return to main menu\n"
                                    "Enter choice: ")
             if patient_option == 'a':
-                # code
+                modify_patient(patient)
             elif patient_option == 'b':
                 medical.Patient.delete_patient(pid)
+                print("Patient deleted.")
                 return
             elif patient_option == 'c':
-                # test
+                add_procedure(patient)
+            elif patient_option == 'd':
+                break
             else:
-                print("Invalid input")
-
+                print("Invalid input [a-d]")
+    else:
+        print("Patient not found.")
 
 def main():
+    """main driver func"""
     while True:
         user_in = get_choice()
         if user_in == '1':
             patient_lookup()
-            print('a')
         elif user_in == '2':
-            # do thing
-            print('b')
+            create_patient()
         elif user_in == '3':
+            print("Data saved. Exiting...")
             medical.Patient.save_patients()
             break
         else:
