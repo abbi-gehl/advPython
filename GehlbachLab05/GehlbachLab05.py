a37ebec0c0f67fc953bf20356046d65ec501b4b5
abbi gehlbach <abigail.gehlbach@mymail.champlain.edu>
2025-02-18 17:01:48 -0500

    matplotlib file

---
+++ GehlbachLab05/GehlbachLab05.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, ax = plt.subplots()                        # Create a figure containing a single Axes.
+ax.plot([1, 2, 3, 4, 5], [1, 4, 2, 3, 1])       # Plot some data on the Axes.
+plt.show()                                      # Show the figure.
+
+
+np.random.seed()  # seed the random number generator.
+data = {'a': np.arange(50),
+        'c': np.random.randint(0, 50, 50),
+        'd': np.random.randn(50)}
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d']) * 100
+
+fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+ax.scatter('a', 'b', c='c', s='d', data=data)
+ax.set_xlabel('entry a')
+ax.set_ylabel('entry b')
+plt.show()
+
+x = np.linspace(0, 2, 100)  # Sample data.
+
+
+fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+ax.plot(x, x, label='linear')
+ax.plot(x, x**2, label='quadratic')
+ax.plot(x, x**3, label='cubic')
+ax.plot(x, np.sqrt(x), label='sqrt')
+
+ax.set_xlabel('x label')  # Add an x-label to the Axes.
+ax.set_ylabel('y label')  # Add a y-label to the Axes.
+ax.set_title("Simple Plot")  # Add a title to the Axes.
+ax.legend()  # Add a legend.
+plt.show()
+
+# use of helper functions:
+
+def my_plotter(ax, data1, data2, param_dict):
+    """
+    A helper function to make a graph.
+    """
+    out = ax.plot(data1, data2, **param_dict)
+    return out
+
+data1, data2, data3, data4 = np.random.randn(4, 100)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(5, 2.7))
+my_plotter(ax1, data1, data2, {'marker': 'x'})
+my_plotter(ax2, data3, data4, {'marker': 'o'})
+plt.show()
+
+fig, ax = plt.subplots(figsize=(5, 2.7))
+x = np.arange(len(data1))
+ax.plot(x, np.cumsum(data1), color='blue', linewidth=3, linestyle='--')
+l, = ax.plot(x, np.cumsum(data2), color='orange', linewidth=2)
+l.set_linestyle(':')
+plt.show()
+
+fig, ax = plt.subplots(figsize=(5, 2.7))
+ax.scatter(data1, data2, s=50, facecolor='C0', edgecolor='k')
+plt.show()
+
+fig, ax = plt.subplots(figsize=(5, 2.7))
+ax.plot(data1, 'o', label='data1')
+ax.plot(data2, 'd', label='data2')
+ax.plot(data3, 'v', label='data3')
+ax.plot(data4, 's', label='data4')
+ax.legend()
+plt.show()
